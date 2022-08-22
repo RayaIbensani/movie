8d68abf225b4c97dd3353f675e72fe7064ee33ed
Qayla Rayani Ibensani <rayaibensani@gmail.com>
2022-08-22 13:49:41 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+const API_KEY = 'api_key=922350dca75addf8982fbb4c9e580b8c';
+const BASE_URL= 'https://api.themoviedb.org/3'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=922350dca75addf8982fbb4c9e580b8c';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+const album =  document.getElementById('album')
+const form =  document.getElementById('form')
+const search =  document.getElementById('search')
+
+let currentPage = 1;
+let nextPage = 2;
+let prevPage = 3;
+let lastUrl = '';
+let totalPages = 100;
+
+getMovie(API_URL);
+
+function getMovie(url){ 
+    lastUrl = url;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        if(data.results.length !== 0){
+            tampilkanMovie(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages; 
+
+            current.innerText = currentPage;
+
+            if(currentPage <= 1){
+              prev.classList.add('disabled');
+              next.classList.remove('disabled')
+            }else if(currentPage>= totalPages){
+              prev.classList.remove('disabled');
+              next.classList.add('disabled')
+            }else{
+              prev.classList.remove('disabled');
+              next.classList.remove('disabled')
+            } 
+            album.scrollIntoView({behavior : 'smooth'})
+
+        }else{
+            album.innerHTML = `<h1 class="no-results" style="color: white;  text-align: center;"> Tidak Ditemukan </h1>`
+        }
+    })
+}
+// Fungsi tampilkan halaman
+function tampilkanMovie(data){
+    album.innerHTML = '';
+            data.forEach(movie => {
+                const {title, poster_path, vote_average, release_date} = movie;
+                album.innerHTML += `
+                    <div class="col-lg-3 col-md-4 col-sm-6 py-2">
+                        <div class="card">
+                            <img src="${poster_path ? IMG_URL + poster_path : "/ ujr5pztc1oitbe7ViMUOilFaJ7s.jpg"}" class="card-img-top" alt="${title}">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: bold;">${title}</h5>
+                                <span class="small">${release_date}</span>
+                                <span class="small">${vote_average}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+                album.appendChild;
+        })
+
+}
+
+
+//fungsi search
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const searchValue = search.value;
+
+    if(searchValue ){
+        getMovie(searchURL+'&query='+searchValue)
+    }else{
+        getMovie(API_URL);
+    }
+
+})
+
+//fungsi next & prev page
+prev.addEventListener('click', () => {
+    if(prevPage > 0){
+      pageCall(prevPage);
+    }
+  })
+  
+  next.addEventListener('click', () => {
+    if(nextPage <= totalPages){
+      pageCall(nextPage);
+    }
+  })
+  
+  function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length -1].split('=');
+    if(key[0] != 'page'){
+      let url = lastUrl + '&page='+page
+      getMovie(url);
+    }else{
+      key[1] = page.toString();
+      let a = key.join('=');
+      queryParams[queryParams.length -1] = a;
+       let b = queryParams.join('&');
+      let url = urlSplit[0] +'?'+ b
+      getMovie(url);
+    }
+  }
